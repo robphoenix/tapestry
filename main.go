df43f6d93930852b71c6b5b4971d725ab95cdcae
Rob Phoenix <rob@robphoenix.com>
2017-05-09 16:38:50 +0100

    correctly marshal JSON

--- main.go
+++ main.go
@@ -3,10 +3,36 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 )
+
+type NodesJSON struct {
+	FabricNodeIdentPol FabricNodeIdentPol `json:"fabricNodeIdentPol"`
+}
+
+type NodeJSON struct {
+	FabricNodeIdentP FabricNodeIdentP `json:"fabricNodeIdentP"`
+}
+
+type FabricNodeIdentPol struct {
+	Attributes Attributes `json:"attributes"`
+	Children   []NodeJSON `json:"children"`
+}
+
+type Attributes struct {
+	Name   string `json:"name,omitempty"`
+	NodeID string `json:"nodeId,omitempty"`
+	Role   string `json:"role,omitempty"`
+	Serial string `json:"serial,omitempty"`
+	Status string `json:"status,omitempty"`
+}
+
+type FabricNodeIdentP struct {
+	Attributes Attributes `json:"attributes"`
+}
 
 func main() {
 	csvFile, err := os.Open(".data/fabric_membership.csv")
@@ -32,5 +58,35 @@ func main() {
 		}
 		nodes = append(nodes, node)
 	}
-	fmt.Printf("%v\n", nodes)
+	// fmt.Printf("%v\n", nodes)
+
+	fnipol := FabricNodeIdentPol{
+		Attributes: Attributes{
+			Status: "created,modified",
+		},
+	}
+	for _, n := range nodes {
+		fnip := NodeJSON{
+			FabricNodeIdentP: FabricNodeIdentP{
+				Attributes: Attributes{
+					Name:   n["Name"],
+					NodeID: n["Node ID"],
+					Role:   n["Role"],
+					Serial: n["Serial"],
+					Status: "created,modified",
+				},
+			}}
+		fnipol.Children = append(fnipol.Children, fnip)
+		fmt.Printf("%q\n", fnip)
+	}
+	// fmt.Printf("%q\n", fnipol)
+	// for _, v := range fnipol.Children {
+	//         fmt.Println(v.Attributes)
+	// }
+	nj := NodesJSON{fnipol}
+	b, err := json.Marshal(nj)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	os.Stdout.Write(b)
 }
