d14fa014d2f149581b1b9784cc00da5555a0ca4a
Rob Phoenix <rob@robphoenix.com>
2017-08-30 15:09:23 +0100

    Implement new command

--- cmd/new.go
+++ cmd/new.go
@@ -15,31 +15,94 @@ package cmd
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 )
 
 // newCmd represents the new command
 var newCmd = &cobra.Command{
-	Use:   "new",
-	Short: "Create a new Tapestry project.",
-	Long:  `New creates a Tapestry project with the given name.`,
-	Args:  cobra.ExactArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("new called")
-	},
+	Use:     "new",
+	Short:   "Create a new Tapestry project.",
+	Long:    `New creates a Tapestry project with the given name.`,
+	Example: "tapestry new <project>",
+	Args:    cobra.ExactArgs(1),
+	Run:     runNew,
 }
+
+func runNew(cmd *cobra.Command, args []string) {
+	dir := filepath.Join(".", args[0])
+
+	if info, _ := os.Stat(dir); info != nil {
+		fmt.Printf("directory already exists: %s\n", dir)
+		return
+	}
+
+	err := os.MkdirAll(dir, 0777)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Create(filepath.Join(dir, "Tapestry.toml"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tmpl, err := template.New("config").Parse(content)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tmpl.Execute(f, args[0])
+
+	fmt.Printf("new Tapestry project created: %s\n", dir)
+}
+
+const content = `
+# {{.}}
+
+# ACI APIC
+[apic]
+url = "example-apic.com"
+username = "admin"
+password = "password"
+
+# Fabric Membership
+[[nodes]]
+id = 101
+name = "leaf-901"
+pod = 1
+serial = "ABCDEF12345"
+role = "leaf"
+
+# Geolocation
+[[sites]]
+name = "site-01"
+description = "Site 01"
+
+[[sites.buildings]]
+name = "building-01"
+description = "Building 01"
+
+[[sites.buildings.floors]]
+name = "floor-01"
+description = "Floor 01"
+
+[[sites.buildings.floors.rooms]]
+name = "room-01"
+description = "Room 01"
+
+[[sites.buildings.floors.rooms.rows]]
+name = "row-01"
+description = "Row 01"
+
+[[sites.buildings.floors.rooms.rows.racks]]
+name = "rack-01"
+description = "Rack 01"
+`
 
 func init() {
 	RootCmd.AddCommand(newCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// newCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// newCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
